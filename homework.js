fd28f8706ce8e3e3bb17994e6aebfd32ccd8bf74
Blossom <70282928+andyviet102@users.noreply.github.com>
2020-09-15 16:32:39 +0700

    Add files via upload

---
+++ homework.js
@@ -0,0 +1,59 @@
+{
+    let message = "Coding is great";
+    console.log(message);
+  }
+  
+  
+  {
+    let studentCount = 0;
+    console.log(studentCount);
+  }
+  
+  {
+    let message;
+    message = "Coding is great";
+    message = "Coding might not be easy, but still great" ;
+    console.log(message);
+  }
+  
+  {
+    let studentCount = 0;
+    studentCount = "0";
+    studentCount = "16";
+    console.log(studentCount);
+  }
+  
+  {
+    let message;
+    message = "coding might not be easy, but still great";
+    console.log(message);
+  }
+  
+  {
+    let studentCount;
+    studentCount = "studentCount++";
+    console.log(studentCount);
+  }
+  
+  alert("have a nice day");
+  let name = prompt("Hi there, your name please?");
+  alert(`Xin chào ${name}, chúc bạn may mắn`);
+  
+  let name1 = prompt("Enter your first name");
+  name2 = prompt("Enter your last name");
+  alert(`Xin chào ${name1} ${name2}`);
+  
+  
+  let a = prompt("Chiều dài cạnh hình vuông");
+  let dientich = a*a ;
+  alert(`Diện tích hình vuông là ${dientich}`);
+  
+  let b = prompt("Chiều dài đường kính");
+  let pi = 3.14
+  let s = b*b*pi
+  alert(`Diện tích hình tròn là ${s}`);
+  
+  let c = prompt("Độ (C)");
+  let f = 9/5*c+32;
+  alert(`(C) = ${f}(F)`);
+
