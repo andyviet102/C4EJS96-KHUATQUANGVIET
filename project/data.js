4cb4e4cf20702d11620e597b9ea419b7286c462b
Blossom <70282928+andyviet102@users.noreply.github.com>
2020-10-12 12:43:09 +0700

    add data.js

---
+++ project/data.js
@@ -0,0 +1,125 @@
+//data brand VETEMENTS
+
+const products =[
+    {   
+        category : `Accessories`,
+                // + Belts
+                // + Hats 
+                // + Scarves
+        name : `GOTHIC EMBROIDERED HAT`,
+        brand : `VETEMENTS`,
+        size : [],
+        color : ['black'],
+        gender : ['unisex'],
+                // man
+                // women
+        material : ['woll'],
+        price : 350,
+        productType : `hats`,
+            // t-shirt, pants , suits ,trousers ,...
+    },
+    {   
+        category : `Accessories`,
+        name : `LOGO MERINO-WOOL SCARF`,
+        brand : `VETEMENTS`,
+        size : [],
+        color : ['white','black'],
+        gender : ['women'],
+        material : ['woll'],
+        price : 750,
+        productType : `scarves`,
+    },
+    {   
+        category : `clothing, coast`,
+        name : `LOGO PRINT RAINCOAT`,
+        brand : `VETEMENTS`,
+        size : ['S','M','L','XL','XXL'],
+        color : ['black'],
+        gender : ['men'],
+        material : ['Synthetic'],
+        price : 1350,
+        productType : `coast`,
+    },
+    {   
+        category : `shoes,`,
+        // boost
+        // sneakers
+        name : `SPIKE RUNNER 2 SNEAKERS`,
+        brand : `VETEMENTS`,
+        size : [7,7.5,8,8.5,9,9.5,10,10.5,11,11.5,12],
+        color : ['orange','red','blue'],
+        gender : ['men','women'],
+        material : ['Synthetic'],
+        price : 590,
+        productType : `shoes, sneakers`,
+    },
+    {   
+        category : `shoes`,
+        name : `BOOMERANG PATENT LEATHER ANKLE BOOTS`,
+        brand : `VETEMENTS`,
+        size : [7,7.5,8,8.5,9,9.5,10,10.5,11,11.5,12],
+        color : ['black',],
+        gender : ['women'],
+        material : ['Synthetic'],
+        price : 1390,
+        productType : `shoes, boots`,
+    },
+    {   
+        category : `clothing, jackets`,
+        name : `REVERSIBLE WHITE & BLACK BOMBER JACKET`,
+        brand : `VETEMENTS`,
+        size : ['S','M','L','XL','XXL'],
+        color : ['black','white'],
+        material : ['Synthetic'],
+        gender : ['women'],
+        price : 1590,
+        productType : `jackets`,
+    },
+    {   
+        category : `clothing, hoodies`,
+        name : `CRYSTAL-EMBELLISHED HOODED SWEATSHIRT IN BLACK`,
+        brand : `VETEMENTS`,
+        size : ['S','M','L','XL','XXL'],
+        color : ['black'],
+        material : ['Synthetic','Cotton'],
+        gender : ['women'],
+        price : 770,
+        productType : `hoodie`,
+    },
+    {   
+        category : `backpacks, bags`,
+        name : `BLACK LOGO BACKPACK`,
+        brand : `VETEMENTS`,
+        size : [],
+        color : ['black',],
+        gender : ['unisex'],
+        material : ['Synthetic'],
+        price : 850,
+        productType : `bags`,
+    },
+    {   
+        category : `clothing, t-shirts`,
+        name : `IT COSTS COTTON T-SHIRT`,
+        brand : `VETEMENTS`,
+        size : ['S','M','L','XL','XXL'],
+        color : ['black'],
+        gender : ['unisex'],
+        material : ['cotton'],
+        price : 190,
+        productType : `t-shirts`,
+    },
+    {   
+        category : `jewelry`,
+        //BRACELET
+        //ring
+        //NECKLACE
+        name : `LOGO-ENGRAVED SIGNET RING`,
+        brand : `VETEMENTS`,
+        size : ['12','13','14','15'],
+        color : ['silver'],
+        gender : ['unisex'],
+        material : ['brass'],
+        price : 300,
+        productType : `ring`,
+    }, 
+]
